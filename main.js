5388b58faf9440d385747bc1b7929c33819c72ea
Michael Victorino <michaelvictorino@gmail.com>
2021-12-10 10:33:06 -0500

    CSS: Changed background color, changed media formats, calculator width

--- main.js
+++ main.js
@@ -4,26 +4,29 @@ var queue = [];
 document.addEventListener('keydown', function(event) {
     if(event.key >= 0 && event.key <= 9)
     {
-        document.getElementById("display").innerHTML+= event.key;
+        document.getElementById("numberdisplay").innerHTML+= event.key;
     }
     if(event.code === 'KeyC')
     {
-        document.getElementById("display").innerHTML = "";
+        document.getElementById("numberdisplay").innerHTML = "";
     }
     if(event.key === '+')
     {
-        var number = document.getElementById("display").innerText;
+        var number = document.getElementById("numberdisplay").innerText;
         queue.push(number);
         //queue.push("+");
-        document.getElementById("display").innerHTML = "";
+        document.getElementById("equationdisplay").innerHTML = number + " + "
+        document.getElementById("numberdisplay").innerHTML = "";
     }
     if(event.key === '=')
     {
-        var number = document.getElementById("display").innerText;
+        var number = document.getElementById("numberdisplay").innerText;
         queue.push(number);
-        document.getElementById("display").innerHTML = "";
-        var num1 = parseInt(queue.shift()) + parseInt(queue.shift());
-        document.getElementById("display").innerHTML = num1;
+        document.getElementById("numberdisplay").innerHTML = "";
+        var num1 = parseInt(queue.shift());
+        var num2 = parseInt(queue.shift());
+        document.getElementById("equationdisplay").innerHTML = num1 + " + " + num2 + " = ";
+        document.getElementById("numberdisplay").innerHTML = num1 + num2;
         
     }
 });
